24d76a53c85dc0b189fa47d5677eadcee14c8471
Will Mason <willmason@live.com>
2022-05-17 21:54:09 +0100

    Update Products Table Seeder

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -19,11 +19,74 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert(
             [
                 [
-                    'name' => 'Brown Chair',
-                    'description' => 'An exquisite brown chair',
+                    'name' => 'Bamboo Mirror',
+                    'description' => 'Bamboo Mirror',
                     'price' => 20,
-                    'category_id' => 1,
-                    'photo' => 'images/cars/cima_1912_top_01.jpg.ximg.l_full_m.smart.jpg',
+                    'category_id' => 5,
+                    'photo' => 'bamboo_mirror_shelf3.JPG',
+                ],
+                [
+                    'name' => 'Green Joiners Drawers',
+                    'description' => 'Green Joiners Drawers',
+                    'price' => 20,
+                    'category_id' => 4,
+                    'photo' => 'green_joiners_drawers5.JPG',
+                ],
+                [
+                    'name' => 'Gypsy Table',
+                    'description' => 'Gypsy Table',
+                    'price' => 20,
+                    'category_id' => 3,
+                    'photo' => 'gypsy_table1.JPG',
+                ],
+                [
+                    'name' => 'Park Signs',
+                    'description' => 'Park Signs',
+                    'price' => 20,
+                    'category_id' => 6,
+                    'photo' => 'Park_signs2.JPG',
+                ],
+                [
+                    'name' => 'Wood & Rattan Fender',
+                    'description' => 'Wood & Rattan Fender',
+                    'price' => 20,
+                    'category_id' => 3,
+                    'photo' => 'fender4.JPG',
+                ],
+                [
+                    'name' => 'Grey Plan Chest',
+                    'description' => 'Grey Plan Chest',
+                    'price' => 20,
+                    'category_id' => 4,
+                    'photo' => 'Grey_plan_chest1.JPG',
+                ],
+                [
+                    'name' => 'Watchmakers Drawers',
+                    'description' => 'Watchmakers Drawers',
+                    'price' => 20,
+                    'category_id' => 4,
+                    'photo' => 'watchmakes_drawers3.JPG',
+                ],
+                [
+                    'name' => 'Painted Drawers',
+                    'description' => 'Painted Drawers',
+                    'price' => 20,
+                    'category_id' => 4,
+                    'photo' => 'Painted_drawers5.JPG',
+                ],
+                [
+                    'name' => 'Treacle Glazed COD',
+                    'description' => 'Treacle Glazed COD',
+                    'price' => 20,
+                    'category_id' => 4,
+                    'photo' => 'treacle_glazed_drawers1.JPG',
+                ],
+                [
+                    'name' => 'Vicar Painting',
+                    'description' => 'Vicar Painting',
+                    'price' => 20,
+                    'category_id' => 6,
+                    'photo' => 'young_vicar_painting2.JPG',
                 ],
             ]
         );
